68a53e5275fb7618caf090fae478ee005d8e3399
Narendra Singh Shekhawat <elitesurfer26@gmail.com>
2015-10-26 11:51:28 +0530

    Delete Question7-Unit1.py

--- Question7-Unit1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# Author: Narendra
-
-'''write a program that can accept two strings as input and concatenate them and then print it in console.'''
-
-first_str = raw_input("Please enter first string: ")
-second_str = raw_input("Please enter second string: ")
-
-print first_str + " " + second_str
