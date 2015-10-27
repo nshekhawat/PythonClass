8d101bed6deee0d2ed56a5a1dac1c76ebd61ff9c
Narendra <nshekhawat@salesforce.com>
2015-10-27 16:35:40 +0530

    Committing.

---
+++ Questions-Unit4/Question6-Unit4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# Author: Narendra
+
+'''
+Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple
+which contains every number. Suppose the following input is supplied to the program: 34,67,55,33,12,98
+Then, the output should be: ['34', '67', '55', '33', '12', '98'] ('34', '67', '55', '33', '12', '98')
+'''
+
+input_data = raw_input("Enter comma separate numbers: ")
+
+list1 = input_data.split(',')
+
+print "{}{}".format(list1,tuple(list1))
---
+++ Questions-Unit4/Question7-Unit4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Author: Narendra
+
+'''
+write a program which takes 2 digits , X,Y as input and generates a 2 dimensional array.
+The element value in the i-th row and j-th column of the array is i*j Note: i=0,1..., X-1; j=0,1,iY-1
+Example: Suppose the following inputs are given to the program: 3,5
+Then, the output of the program should be: [[0,0,0,0,0]],[0,1,2,3,4],[0,2,4,6,8]]
+'''
+
+X = int(raw_input("Enter first digit X: "))
+Y = int(raw_input("Enter second digit Y: "))
+
+list1 = []
+
+for i in range(0,X):
+    list1.append([])
+    for j in range(0,Y):
+        list1[i].append(i*j)
+
+print list1
