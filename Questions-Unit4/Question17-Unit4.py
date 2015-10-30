2066d1875ea35016d50ba73bd660b2e99d437607
Narendra Singh Shekhawat <nshekhawat@nshekhwat1-wsl.internal.salesforce.com>
2015-10-30 11:27:00 +0530

    Adding Question17 Answer.

---
+++ Questions-Unit4/Question17-Unit4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# Author: Narendra
+
+'''
+Write a program which can map() and filter() to make a list whose elements are square of even number in
+
+[1,2,3,4,5,6,7,8,9,10].
+'''
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+list2 = map(lambda x: x**2, list1)
+
+print filter(lambda y: y % 2 == 0, list2)
